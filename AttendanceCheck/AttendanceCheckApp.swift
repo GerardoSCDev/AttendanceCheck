659516881049110bc6b8dca6a2f9b44a14ec3c7b
Gerardo Santillan <gerardo.sc.dev@gmail.com>
2025-05-08 11:02:51 -0600

    Initial Commit

---
+++ AttendanceCheck/AttendanceCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttendanceCheckApp.swift
+//  AttendanceCheck
+//
+//  Created by Gerardo Santillan Cruzado on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AttendanceCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
